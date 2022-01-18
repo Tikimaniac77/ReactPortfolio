e3d08465844e2233c9a02473f0237af134729b80
Tiki <michaelwachtel@yahoo.com>
2022-01-18 12:57:02 -0800

    Updates

---
+++ precache-manifest.7928a8a3d8f3e312a277c528139cf60e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259b3b5d8a18878a78db346e8782a22f",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "f7f5f265c923274ade2c",
+    "url": "/ReactPortfolio/static/css/2.45643e88.chunk.css"
+  },
+  {
+    "revision": "94ec6538d961bac9bc6c",
+    "url": "/ReactPortfolio/static/css/main.e82235aa.chunk.css"
+  },
+  {
+    "revision": "f7f5f265c923274ade2c",
+    "url": "/ReactPortfolio/static/js/2.bed005fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.bed005fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94ec6538d961bac9bc6c",
+    "url": "/ReactPortfolio/static/js/main.d493be55.chunk.js"
+  },
+  {
+    "revision": "97a1c7d0b592075495b7",
+    "url": "/ReactPortfolio/static/js/runtime-main.ad87311a.js"
+  }
+]);
