845077a9ed093743fa583bc1a9af983ec825225f
Tiki <michaelwachtel@yahoo.com>
2022-03-03 14:08:28 -0800

    updated about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -21,23 +21,29 @@ function About() {
 
           <p>
             Front-end web developer with a background in entrepreneurial
-            business administration and 5+ years experience working in the
+            business administration and 6+ years of experience working in the
             software industry. Pursuing an innovative team to leverage my newly
-            developed skills in JavaScript, CSS, React.js, and responsive web
-            design. Considered to have an excellent analytical eye for solutions
-            development on the administrative side, I’m now eager and excited to
-            apply my new knowledge towards the development sector by leveraging
-            my upcoming full stack development certificate. With any project, my
-            goal is to provide the best possible end user satisfaction, be it an
-            internal or external customer, the user experience is always the
-            keystone. I’m currently working closely with a four person team
-            developing an application that bridges and aggregates multiple API’s
-            into one easily available and user friendly resource. It’s goal is
-            to help singles locate potential partners by matching common
-            interests and suggest date locations based on the entered
-            information gathered. I’m anxious and eager to continue applying my
-            skills as part of an ever changing, quality-centric team, crafting
-            the highest quality of user experience on the web.
+            developed skills in: Browser Based Technologies: • HTML/CSS •
+            JavaScript/jQuery • Responsive Design • Bootstrap Handlebars • Local
+            Storage, Session Storage, IndexedDB • React.js Databases: • MySQL •
+            MongoDB Server-Side Development: • User Authentication • Progressive
+            Web Applications (PWAs) • MERN Stack (MongoDB, Express.js, React.js,
+            Node.js) API Interaction: • API • JSON • AJAX Considered to have an
+            excellent analytical eye for solutions development on the
+            administrative side, I’m now eager and excited to apply my new
+            knowledge towards the development sector by leveraging my upcoming
+            full stack development certificate. With any project, my goal is to
+            provide the best possible end user satisfaction, be it an internal
+            or external customer, the user experience is always the keystone.
+            I’ve worked closely with a four person teams developing applications
+            such Scrappi; a service to help people quickly parse though data and
+            produce a short or long analysis using an open-source machine
+            learning (AI) library from TensorFlow. First you can present one or
+            multiple documents, determine the file type, and then, you will be
+            presented with an analysis that you can either save to your profile
+            or delete. I’m anxious and eager to continue applying my skills as
+            part of an ever changing, quality-centric team, crafting the highest
+            quality of user experience on the web
           </p>
           <p>
             View full{" "}
